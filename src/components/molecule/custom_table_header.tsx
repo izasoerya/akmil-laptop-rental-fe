fda30f0f8e399bc39d8b254eef40709b9e262554
izasoerya <ihza.surya@gmail.com>
2025-10-01 01:25:46 +0700

    feat: ui improvement

--- src/components/molecule/custom_table_header.tsx
+++ src/components/molecule/custom_table_header.tsx
@@ -11,16 +11,22 @@ import {
   Field,
   Input,
   Dialog,
+  Icon,
 } from "@chakra-ui/react";
 import { CgAdd } from "react-icons/cg";
 import SupabaseService from "@/services/supabase_service";
+import { FaSun, FaMoon } from "react-icons/fa";
 
 interface CustomTableHeaderProps {
   lastUpdate?: string;
+  toggleDarkMode: () => void;
+  isDarkMode: boolean;
 }
 
 const CustomTableHeader: React.FC<CustomTableHeaderProps> = ({
   lastUpdate,
+  toggleDarkMode,
+  isDarkMode,
 }) => {
   const [laptopName, setLaptopName] = useState("");
 
@@ -35,30 +41,53 @@ const CustomTableHeader: React.FC<CustomTableHeaderProps> = ({
   };
 
   return (
-    <Box bg="gray.800" px={10} py={4} borderRadius="md" boxShadow="sm">
+    <Box
+      px={10}
+      py={4}
+      borderRadius="md"
+      boxShadow={
+        isDarkMode
+          ? "0 40px 60px rgba(0, 0, 0, 0.1)"
+          : "0 4px 6px rgba(0, 0, 0, 0.05)"
+      }
+      style={{
+        width: "75%",
+        margin: "auto",
+        // Glassmorphism styles applied here
+        background: isDarkMode
+          ? "rgba(0, 0, 0, 0.5)" // Dark, semi-transparent
+          : "rgba(255, 255, 255, 0.35)", // Light, semi-transparent
+        backdropFilter: "blur(12px)",
+        WebkitBackdropFilter: "blur(12px)", // For Safari support
+      }}
+    >
       <Flex
         justify="space-between"
         align="center"
         direction={{ base: "column", md: "row" }}
         gap={4}
       >
-        <Text fontSize="xl" fontWeight="bold" color="white">
+        <Text
+          fontSize="xl"
+          fontWeight="bold"
+          color={isDarkMode ? "#f9f9f9" : "#181818ff"} // Adjusted text color for better contrast
+        >
           RealTime Dashboard
         </Text>
-        <Flex gap="5">
+        <Flex gap="5" align="center">
           <VStack align="flex-end" gap={0}>
-            <Text fontSize="sm" color="white">
+            <Text fontSize="sm" color={isDarkMode ? "white" : "black"}>
               Last update:
             </Text>
-            <Text fontSize="sm" color="white">
+            <Text fontSize="sm" color={isDarkMode ? "white" : "black"}>
               {lastUpdate ? lastUpdate : "-"}
             </Text>
           </VStack>
 
           <Dialog.Root>
             <Dialog.Trigger asChild>
-              <IconButton aria-label="Add laptop">
-                <CgAdd color="white" />
+              <IconButton aria-label="Add laptop" bg="gray.900" color="white">
+                <CgAdd color={"white"} />
               </IconButton>
             </Dialog.Trigger>
             <Portal>
@@ -95,6 +124,17 @@ const CustomTableHeader: React.FC<CustomTableHeaderProps> = ({
               </Dialog.Positioner>
             </Portal>
           </Dialog.Root>
+
+          <Button
+            onClick={toggleDarkMode}
+            colorScheme={isDarkMode ? "yellow" : "blue"}
+          >
+            {isDarkMode ? (
+              <Icon as={FaSun} color="white" />
+            ) : (
+              <Icon as={FaMoon} color="white" />
+            )}
+          </Button>
         </Flex>
       </Flex>
     </Box>
--- src/components/ui/dashboard.tsx
+++ src/components/ui/dashboard.tsx
@@ -20,6 +20,16 @@ import ListName from "../../models/lookup_table_name";
 function Dashboard() {
   const [items, setItems] = useState<LaptopAcc[]>([]);
   const [loading, setLoading] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const containerStyle = {
+    background: isDarkMode
+      ? "linear-gradient(135deg, #222 0%, #444 100%)"
+      : "linear-gradient(135deg, #1976d2 0%, #e3f2fd 100%)",
+    color: isDarkMode ? "#fff" : "#000",
+    minHeight: "100vh",
+    padding: "1rem",
+  };
 
   useEffect(() => {
     let unsub: { unsubscribe: () => void } | null = null;
@@ -68,105 +78,203 @@ function Dashboard() {
     try {
       await supabaseService.deleteLaptopAcc(id);
     } catch (err) {
-      console.error("Insert error:", err);
+      console.error("Delete error:", err);
     }
   };
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={containerStyle}>
       <Flex gap="2" direction="column">
-        <CustomTableHeader lastUpdate={new Date().toLocaleString()} />
+        <CustomTableHeader
+          lastUpdate={new Date().toLocaleString()}
+          toggleDarkMode={() => setIsDarkMode((prev) => !prev)}
+          isDarkMode={isDarkMode}
+        />
 
         {loading ? (
           <Spinner size="xl" alignSelf="center" />
         ) : (
-          <Table.Root size="md" interactive>
-            <Table.Header>
-              <Table.Row>
-                <Table.ColumnHeader>Product</Table.ColumnHeader>
-                <Table.ColumnHeader>Last Interact</Table.ColumnHeader>
-                <Table.ColumnHeader>Rented By</Table.ColumnHeader>
-                <Table.ColumnHeader textAlign="center">
-                  Status
-                </Table.ColumnHeader>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {items.map((item) => (
-                <Table.Row key={item.id}>
-                  <Table.Cell>{item.name}</Table.Cell>
-                  <Table.Cell>
-                    {item.last_rented
-                      ? new Date(item.last_rented).toLocaleString("en-US", {
-                          year: "numeric",
-                          month: "short",
-                          day: "numeric",
-                          hour: "2-digit",
-                          minute: "2-digit",
-                        })
-                      : "-"}
-                  </Table.Cell>
-                  <Table.Cell>
-                    {item.user_id
-                      ? (ListName as { [index: number]: string })[item.user_id]
-                      : "-"}
-                  </Table.Cell>
-                  <Table.Cell justifyItems={"center"}>
-                    <Flex gap="2" align="center">
-                      <Box
-                        borderRadius="md"
-                        fontWeight="bold"
-                        color={item.user_id ? "white" : "green.100"}
-                        bg={item.user_id ? "red.600" : "green.700"}
-                        boxShadow="sm"
-                        display="flex"
-                        alignItems="center"
-                        minWidth="80px"
-                        height="40px"
-                        justifyContent="center"
-                      >
-                        {item.user_id ? "Rented" : "Available"}
-                      </Box>
-                      <Dialog.Root>
-                        <Dialog.Trigger asChild>
-                          <IconButton bg="red">
-                            <BiTrash color="white"></BiTrash>
-                          </IconButton>
-                        </Dialog.Trigger>
-                        <Portal>
-                          <Dialog.Backdrop />
-                          <Dialog.Positioner>
-                            <Dialog.Content>
-                              <Dialog.Header>
-                                <Dialog.Title>Delete Item</Dialog.Title>
-                              </Dialog.Header>
-                              <Dialog.Body>
-                                <p>Are you sure? This is irreversible.</p>
-                              </Dialog.Body>
-                              <Dialog.Footer>
-                                <Dialog.ActionTrigger asChild>
-                                  <Button variant="outline">Cancel</Button>
-                                </Dialog.ActionTrigger>
-                                <Button
-                                  color="white"
-                                  onClick={() => handleDelete(Number(item.id))}
-                                >
-                                  Delete
-                                </Button>
-                              </Dialog.Footer>
-                              <Dialog.CloseTrigger asChild>
-                                <CloseButton size="sm" />
-                              </Dialog.CloseTrigger>
-                            </Dialog.Content>
-                          </Dialog.Positioner>
-                        </Portal>
-                      </Dialog.Root>
-                    </Flex>
-                  </Table.Cell>
+          <div
+            style={{
+              width: "75%",
+              margin: "auto",
+              background: isDarkMode
+                ? "rgba(0, 0, 0, 0.5)"
+                : "rgba(255, 255, 255, 0.35)",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              boxShadow: "0 8px 24px rgba(0, 0, 0, 0.25)",
+              backdropFilter: "blur(12px)",
+              WebkitBackdropFilter: "blur(12px)",
+              overflow: "hidden",
+              padding: "2rem",
+              borderTopLeftRadius: "12px",
+              borderBottomLeftRadius: "12px",
+              borderTopRightRadius: "12px",
+              borderBottomRightRadius: "12px",
+            }}
+          >
+            <Table.Root
+              size="md"
+              interactive
+              style={{
+                width: "100%",
+                height: "100%",
+                borderCollapse: "separate",
+                borderSpacing: "0 10px",
+                background: "transparent",
+                color: isDarkMode ? "#fff" : "#000",
+              }}
+            >
+              <Table.Header>
+                <Table.Row>
+                  <Table.ColumnHeader
+                    style={{
+                      borderTopLeftRadius: "12px",
+                      borderBottomLeftRadius: "12px",
+                    }}
+                  >
+                    Product
+                  </Table.ColumnHeader>
+                  <Table.ColumnHeader>Last Interact</Table.ColumnHeader>
+                  <Table.ColumnHeader>Rented By</Table.ColumnHeader>
+                  <Table.ColumnHeader
+                    textAlign="center"
+                    style={{
+                      borderTopRightRadius: "12px",
+                      borderBottomRightRadius: "12px",
+                    }}
+                  >
+                    Status
+                  </Table.ColumnHeader>
                 </Table.Row>
-              ))}
-            </Table.Body>
-          </Table.Root>
+              </Table.Header>
+              <Table.Body style={{ background: "transparent" }}>
+                {items.map((item) => (
+                  <Table.Row
+                    key={item.id}
+                    style={{ background: "transparent" }}
+                  >
+                    {/* First Cell */}
+                    <Table.Cell
+                      style={{
+                        background: isDarkMode
+                          ? "rgba(51, 51, 51, 0.4)"
+                          : "#F0F8FF",
+                        border: "none",
+                        borderTopLeftRadius: "12px",
+                        borderBottomLeftRadius: "12px",
+                      }}
+                    >
+                      {item.name}
+                    </Table.Cell>
+
+                    {/* Middle Cells */}
+                    <Table.Cell
+                      style={{
+                        background: isDarkMode
+                          ? "rgba(51, 51, 51, 0.4)"
+                          : "#F0F8FF",
+                        border: "none",
+                      }}
+                    >
+                      {item.last_rented
+                        ? new Date(item.last_rented).toLocaleString("en-US", {
+                            year: "numeric",
+                            month: "short",
+                            day: "numeric",
+                            hour: "2-digit",
+                            minute: "2-digit",
+                          })
+                        : "-"}
+                    </Table.Cell>
+                    <Table.Cell
+                      style={{
+                        background: isDarkMode
+                          ? "rgba(51, 51, 51, 0.4)"
+                          : "#F0F8FF",
+                        border: "none",
+                      }}
+                    >
+                      {item.user_id
+                        ? (ListName as { [index: number]: string })[
+                            item.user_id
+                          ]
+                        : "-"}
+                    </Table.Cell>
+
+                    {/* Last Cell */}
+                    <Table.Cell
+                      style={{
+                        background: isDarkMode
+                          ? "rgba(51, 51, 51, 0.4)"
+                          : "#F0F8FF",
+                        border: "none",
+                        borderTopRightRadius: "12px",
+                        borderBottomRightRadius: "12px",
+                        display: "flex",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Flex gap="2" align="center">
+                        <Box
+                          borderRadius="md"
+                          fontWeight="bold"
+                          color={item.user_id ? "white" : "green.100"}
+                          bg={item.user_id ? "red.600" : "green.700"}
+                          boxShadow="sm"
+                          display="flex"
+                          alignItems="center"
+                          minWidth="80px"
+                          height="40px"
+                          justifyContent="center"
+                        >
+                          {item.user_id ? "Rented" : "Available"}
+                        </Box>
+                        <Dialog.Root>
+                          <Dialog.Trigger asChild>
+                            <IconButton bg="red">
+                              <BiTrash color="white" />
+                            </IconButton>
+                          </Dialog.Trigger>
+                          <Portal>
+                            <Dialog.Backdrop />
+                            <Dialog.Positioner>
+                              <Dialog.Content>
+                                <Dialog.Header>
+                                  <Dialog.Title>Delete Item</Dialog.Title>
+                                </Dialog.Header>
+                                <Dialog.Body>
+                                  <p>Are you sure? This is irreversible.</p>
+                                </Dialog.Body>
+                                <Dialog.Footer>
+                                  <Dialog.ActionTrigger asChild>
+                                    <Button variant="outline">Cancel</Button>
+                                  </Dialog.ActionTrigger>
+                                  <Button
+                                    color="white"
+                                    onClick={() =>
+                                      handleDelete(Number(item.id))
+                                    }
+                                  >
+                                    Delete
+                                  </Button>
+                                </Dialog.Footer>
+                                <Dialog.CloseTrigger asChild>
+                                  <CloseButton size="sm" />
+                                </Dialog.CloseTrigger>
+                              </Dialog.Content>
+                            </Dialog.Positioner>
+                          </Portal>
+                        </Dialog.Root>
+                      </Flex>
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table.Root>
+          </div>
         )}
       </Flex>
     </div>
